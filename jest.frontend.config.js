c1b35af2947bcdebc6c9f215bde1f673634339a4
qinxutan <122926007+qinxutan@users.noreply.github.com>
2026-02-03 00:04:18 +0800

    update

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -22,7 +22,6 @@ export default {
   // only run these tests
   testMatch: [
     "<rootDir>/client/src/pages/Auth/*.test.js",
-    "<rootDir>/client/src/context/*.test.js",
     "<rootDir>/client/src/pages/*.test.js",
   ],
 
