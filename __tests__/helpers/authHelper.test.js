1b142a45bc93c2c0814465e67adc9826f2ee4c28
mahadhir247 <mahadhir123456@gmail.com>
2026-02-07 18:25:24 +0800

    Add unit test for comparePassword

--- __tests__/helpers/authHelper.test.js
+++ __tests__/helpers/authHelper.test.js
@@ -1,29 +1,41 @@
 import bcrypt from "bcrypt";
-import { hashPassword } from "../../helpers/authHelper";
+import { hashPassword, comparePassword } from "../../helpers/authHelper";
 import { describe } from "node:test";
 
 jest.mock("bcrypt");
 
-describe("hashPassword", () => {
-    it("should return hashed password", async () => {
-        const mockHash = "mockhashedvalue";
-        bcrypt.hash.mockResolvedValue(mockHash);
+describe("authHelper Tests", () => {
+    describe("hashPassword", () => {
+        it("should return hashed password", async () => {
+            const mockHash = "mockhashedvalue";
+            bcrypt.hash.mockResolvedValue(mockHash);
 
-        const result = await hashPassword("testPassword");
+            const result = await hashPassword("testPassword");
 
-        expect(bcrypt.hash).toHaveBeenCalledWith("testPassword", 10);
-        expect(result).toBe(mockHash);
+            expect(bcrypt.hash).toHaveBeenCalledWith("testPassword", 10);
+            expect(result).toBe(mockHash);
+        });
+
+        it("should log error if internal error is thrown", async () => {
+            const mockError = new Error("mock-error");
+            const consoleSpy = jest.spyOn(global.console, 'log');
+            bcrypt.hash.mockImplementation(() => { throw mockError });
+
+            await hashPassword("testPassword");
+
+            expect(consoleSpy).toHaveBeenCalledWith(mockError);
+
+            bcrypt.hash.mockReset();
+        });
     });
 
-    it("should log error if internal error is thrown", async () => {
-        const mockError = new Error("mock-error");
-        const consoleSpy = jest.spyOn(global.console, 'log');
-        bcrypt.hash.mockImplementation(() => { throw mockError });
+    describe("comparePassword", () => {
+        it("should call compare with correct arguments", async () => {
+            const spy = jest.spyOn(bcrypt, 'compare');
+            
+            await comparePassword("password", "hashPassword");
 
-        await hashPassword("testPassword");
-
-        expect(consoleSpy).toHaveBeenCalledWith(mockError);
-
-        bcrypt.hash.mockReset();
-    })
+            expect(spy).toHaveBeenCalledWith("password", "hashPassword");
+        });
+    });
 });
