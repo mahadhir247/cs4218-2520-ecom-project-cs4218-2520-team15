3a14c49cfa407cabe5262b38a615ed9eca1abcd7
mahadhir247 <mahadhir123456@gmail.com>
2026-02-08 17:42:07 +0800

    Add unit tests for Private.js
    Added unit tests for private routing

---
+++ client/src/__tests__/components/Routes/Private.test.js
@@ -0,0 +1,60 @@
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import PrivateRoute from "../../../components/Routes/Private";
+import { useAuth } from "../../../context/auth";
+import axios from "axios";
+import { render, waitFor } from "@testing-library/react";
+
+jest.mock("axios");
+jest.mock("@context/auth", () => ({
+    useAuth: jest.fn(() => [null, jest.fn()]),
+}));
+jest.mock("@components/Spinner", () => ({ path }) => (
+  <div>spinner-mock</div>
+));
+jest.mock("mongoose", () => ({
+    set: jest.fn(),
+}));
+
+function renderPrivateRoute() {
+    return render(
+        <MemoryRouter initialEntries={["/private"]}>
+            <Routes>
+                <Route path="/private" element={<PrivateRoute />}>
+                    <Route path="/private" element={<div>child-element</div>} />
+                </Route>
+            </Routes>
+        </MemoryRouter>
+    )
+}
+
+describe("Private tests", () => {
+    it("should render spinner when there is no auth token", async () => {
+        const { getByText } = renderPrivateRoute();
+
+        expect(getByText("spinner-mock")).toBeInTheDocument();
+    });
+
+    it("should render spinner if auth token is not ok", async () => {
+        useAuth.mockReturnValue([{ token: "mockToken" }, jest.fn()]);
+        axios.get.mockResolvedValue({ data: { ok: false } });
+
+        const { getByText } = renderPrivateRoute();
+
+        await waitFor(() => {
+            expect(getByText("spinner-mock")).toBeInTheDocument();
+        });
+        expect(axios.get).toHaveBeenCalled();
+    });
+
+    it("should render outlet child if auth token is ok", async () => {
+        useAuth.mockReturnValue([{ token: "mockToken" }, jest.fn()]);
+        axios.get.mockResolvedValue({ data: { ok: true } });
+
+        const { getByText } = renderPrivateRoute();
+
+        await waitFor(() => {
+            expect(getByText("child-element")).toBeInTheDocument();
+        });
+        expect(axios.get).toHaveBeenCalled();
+    });
+});
