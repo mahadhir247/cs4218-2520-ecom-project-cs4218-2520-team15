3fb6d9148a6dccb4ac346da4c07ffb1501b52aa1
mahadhir247 <mahadhir123456@gmail.com>
2026-02-08 16:26:20 +0800

    Add unit test for Dashboard.js

---
+++ client/src/__tests__/pages/user/Dashboard.test.js
@@ -0,0 +1,44 @@
+const { render } = require("@testing-library/react");
+const { default: Dashboard } = require("../../../pages/user/Dashboard");
+const { useAuth } = require("../../../context/auth");
+
+jest.mock("@components/Layout", () => ({ children, ...props }) => (
+  <div {...props}>{children}</div>
+));
+jest.mock("@components/UserMenu");
+jest.mock("@context/auth", () => ({ useAuth: jest.fn() }));
+
+describe("Dashboard page", () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+
+    it("should render user details correctly", () => {
+        useAuth.mockReturnValue([
+            {
+                user: {
+                    name: "Test",
+                    email: "test@gmail.com",
+                    address: "testAddress",
+                },
+            }, 
+            jest.fn()
+        ]);
+
+        const { getByText } = render(<Dashboard />);
+
+        expect(getByText("Name: Test", { exact: true })).toBeInTheDocument();
+        expect(getByText("Email: test@gmail.com", { exact: true })).toBeInTheDocument();
+        expect(getByText("Address: testAddress", { exact: true })).toBeInTheDocument();
+    });
+
+    it("should render page if user details is missing", () => {
+        useAuth.mockReturnValue([null, jest.fn()]);
+
+        const { getByText } = render(<Dashboard />);
+
+        expect(getByText("Name:", { exact: true })).toBeInTheDocument();
+        expect(getByText("Email:", { exact: true })).toBeInTheDocument();
+        expect(getByText("Address:", { exact: true })).toBeInTheDocument();
+    })
+});
