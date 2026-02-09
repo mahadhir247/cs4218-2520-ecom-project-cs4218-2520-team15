4e3ec08c27f87075228b39791f1412704836d6b0
miljyy <miljy123@gmail.com>
2026-02-09 14:42:32 +0800

    Standardise model naming convention

--- models/orderModel.js
+++ models/orderModel.js
@@ -5,18 +5,18 @@ const orderSchema = new mongoose.Schema(
     products: [
       {
         type: mongoose.ObjectId,
-        ref: "Products",
+        ref: "Product",
       },
     ],
     payment: {},
     buyer: {
       type: mongoose.ObjectId,
-      ref: "users",
+      ref: "User",
     },
     status: {
       type: String,
-      default: "Not Process",
-      enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"],
+      default: "Not Processed",
+      enum: ["Not Processed", "Processing", "Shipped", "Delivered", "Cancelled"],
     },
   },
   { timestamps: true }
