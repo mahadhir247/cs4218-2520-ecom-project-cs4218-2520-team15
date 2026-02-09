07c88be8a5387c9b09feb6a3c8f06570362b4b05
Inez Kok <inezkok@r-182-104-25-172.comp.nus.edu.sg>
2026-02-09 16:00:40 +0800

    Fix typo from class to className

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -69,7 +69,7 @@ const ProductDetails = () => {
           </h6>
           <h6>Category : {product?.category?.name}</h6>
           <button 
-            class="btn btn-secondary ms-1"
+            className="btn btn-secondary ms-1"
             onClick={() => {
               setCart([...cart, product]);
               localStorage.setItem(
