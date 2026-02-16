8ebd6c87ec03d6d5c48044865040a206e2fd68c7
delonlee01 <delonlee2001@hotmail.com>
2026-02-16 13:23:37 +0800

    Fix bugs in CreateProduct page

--- client/src/pages/admin/CreateProduct.js
+++ client/src/pages/admin/CreateProduct.js
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from "react";
-import Layout from "./../../components/Layout";
-import AdminMenu from "./../../components/AdminMenu";
-import toast from "react-hot-toast";
-import axios from "axios";
 import { Select } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import AdminMenu from "./../../components/AdminMenu";
+import Layout from "./../../components/Layout";
+
 const { Option } = Select;
 
 const CreateProduct = () => {
   const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [category, setCategory] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [shipping, setShipping] = useState("");
-  const [photo, setPhoto] = useState("");
+  const [formValue, setFormValue] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "",
+    quantity: "",
+    shipping: "",
+    photo: "",
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
 
   //get all category
   const getAllCategory = async () => {
@@ -24,10 +28,12 @@ const CreateProduct = () => {
       const { data } = await axios.get("/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
+      } else {
+        setCategories([]);
       }
     } catch (error) {
       console.log(error);
-      toast.error("Something wwent wrong in getting catgeory");
+      toast.error("Something went wrong in getting category");
     }
   };
 
@@ -35,36 +41,49 @@ const CreateProduct = () => {
     getAllCategory();
   }, []);
 
+  const updateField = (field, value) => {
+    if (field == "photo" && value?.size) {
+      if (value.size > 1000000) {
+        toast.error("Photo size cannot exceed 1MB");
+        return;
+      }
+    }
+    const updatedFormValue = { ...formValue, [field]: value };
+    setFormValue(updatedFormValue);
+    const isValid = Object.values(updatedFormValue).every((value) => !!value);
+    setIsFormValid(isValid);
+  };
+
   //create product function
   const handleCreate = async (e) => {
     e.preventDefault();
     try {
       const productData = new FormData();
-      productData.append("name", name);
-      productData.append("description", description);
-      productData.append("price", price);
-      productData.append("quantity", quantity);
-      productData.append("photo", photo);
-      productData.append("category", category);
-      const { data } = axios.post(
+      productData.append("name", formValue.name);
+      productData.append("description", formValue.description);
+      productData.append("price", formValue.price);
+      productData.append("quantity", formValue.quantity);
+      productData.append("photo", formValue.photo);
+      productData.append("category", formValue.category);
+      const { data } = await axios.post(
         "/api/v1/product/create-product",
-        productData
+        productData,
       );
       if (data?.success) {
-        toast.error(data?.message);
-      } else {
-        toast.success("Product Created Successfully");
+        toast.success("Product created successfully");
         navigate("/dashboard/admin/products");
+      } else {
+        toast.error(data?.message);
       }
     } catch (error) {
       console.log(error);
-      toast.error("something went wrong");
+      toast.error("Something went wrong");
     }
   };
 
   return (
     <Layout title={"Dashboard - Create Product"}>
-      <div className="container-fluid m-3 p-3">
+      <div className="container-fluid p-3">
         <div className="row">
           <div className="col-md-3">
             <AdminMenu />
@@ -73,13 +92,13 @@ const CreateProduct = () => {
             <h1>Create Product</h1>
             <div className="m-1 w-75">
               <Select
-                bordered={false}
+                variant="borderless"
                 placeholder="Select a category"
                 size="large"
                 showSearch
                 className="form-select mb-3"
                 onChange={(value) => {
-                  setCategory(value);
+                  updateField("category", value);
                 }}
               >
                 {categories?.map((c) => (
@@ -90,21 +109,21 @@ const CreateProduct = () => {
               </Select>
               <div className="mb-3">
                 <label className="btn btn-outline-secondary col-md-12">
-                  {photo ? photo.name : "Upload Photo"}
+                  {formValue.photo ? formValue.photo.name : "Upload Photo"}
                   <input
                     type="file"
                     name="photo"
                     accept="image/*"
-                    onChange={(e) => setPhoto(e.target.files[0])}
+                    onChange={(e) => updateField("photo", e.target.files[0])}
                     hidden
                   />
                 </label>
               </div>
               <div className="mb-3">
-                {photo && (
+                {formValue.photo && (
                   <div className="text-center">
                     <img
-                      src={URL.createObjectURL(photo)}
+                      src={URL.createObjectURL(formValue.photo)}
                       alt="product_photo"
                       height={"200px"}
                       className="img img-responsive"
@@ -115,58 +134,65 @@ const CreateProduct = () => {
               <div className="mb-3">
                 <input
                   type="text"
-                  value={name}
-                  placeholder="write a name"
+                  value={formValue.name}
+                  placeholder="Enter a name"
                   className="form-control"
-                  onChange={(e) => setName(e.target.value)}
+                  onChange={(e) => updateField("name", e.target.value)}
                 />
               </div>
               <div className="mb-3">
                 <textarea
                   type="text"
-                  value={description}
-                  placeholder="write a description"
+                  value={formValue.description}
+                  placeholder="Enter a description"
                   className="form-control"
-                  onChange={(e) => setDescription(e.target.value)}
-                />
-              </div>
-
-              <div className="mb-3">
-                <input
-                  type="number"
-                  value={price}
-                  placeholder="write a Price"
-                  className="form-control"
-                  onChange={(e) => setPrice(e.target.value)}
+                  onChange={(e) => updateField("description", e.target.value)}
                 />
               </div>
               <div className="mb-3">
                 <input
                   type="number"
-                  value={quantity}
-                  placeholder="write a quantity"
+                  value={formValue.price}
+                  placeholder="Enter a price"
                   className="form-control"
-                  onChange={(e) => setQuantity(e.target.value)}
+                  onChange={(e) => updateField("price", e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={formValue.quantity}
+                  placeholder="Enter a quantity"
+                  className="form-control"
+                  onChange={(e) => updateField("quantity", e.target.value)}
                 />
               </div>
               <div className="mb-3">
                 <Select
-                  bordered={false}
-                  placeholder="Select Shipping "
+                  variant="borderless"
+                  placeholder="Select shipping"
                   size="large"
                   showSearch
                   className="form-select mb-3"
                   onChange={(value) => {
-                    setShipping(value);
+                    updateField("shipping", value);
                   }}
                 >
-                  <Option value="0">No</Option>
-                  <Option value="1">Yes</Option>
+                  <Option key={0} value="0">
+                    No
+                  </Option>
+                  <Option key={1} value="1">
+                    Yes
+                  </Option>
                 </Select>
               </div>
               <div className="mb-3">
-                <button className="btn btn-primary" onClick={handleCreate}>
-                  CREATE PRODUCT
+                <button
+                  className="btn btn-primary"
+                  onClick={handleCreate}
+                  disabled={!isFormValid}
+                >
+                  Create
                 </button>
               </div>
             </div>
