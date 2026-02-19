04e6ee05254b6a62f3015ebe76e182d593349707
delonlee01 <delonlee2001@hotmail.com>
2026-02-19 16:53:21 +0800

    Add tests for updateProductController function

--- __tests__/controllers/product/productController.write.test.js
+++ __tests__/controllers/product/productController.write.test.js
@@ -1,4 +1,7 @@
-import { createProductController } from "../../../controllers/productController";
+import {
+  createProductController,
+  updateProductController,
+} from "../../../controllers/productController";
 
 jest.mock(
   "slugify",
@@ -370,6 +373,502 @@ describe("createProductController function", () => {
           category: "1",
           quantity: "10",
           shipping: 0,
+          photo: {
+            data: "mock-file",
+            contentType: "image/png",
+          },
+        },
+      });
+    });
+  });
+});
+
+describe("updateProductController function", () => {
+  const mockRes = {
+    status: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
+  };
+  const modelSaveWrapper = (data) => {
+    const product = { ...data };
+    Object.defineProperty(product, "save", {
+      value: jest.fn(),
+      enumerable: false,
+    });
+    return product;
+  };
+
+  beforeAll(() => {
+    jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should update product correctly", async () => {
+    const mockReq = {
+      params: { pid: "1" },
+      fields: {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+      },
+      files: {
+        photo: {
+          path: "/path/mock-file-1.png",
+          type: "image/png",
+          size: 100,
+        },
+      },
+    };
+    productModel.findByIdAndUpdate.mockResolvedValueOnce(
+      modelSaveWrapper({
+        _id: "1",
+        name: "Computer",
+        description: "A mock computer",
+        price: 10.99,
+        category: "1",
+        quantity: 10,
+        shipping: true,
+        photo: {
+          data: "mock-file-1",
+          contentType: "image/png",
+        },
+      }),
+    );
+
+    await updateProductController(mockReq, mockRes);
+
+    expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      "1",
+      {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+        slug: "computer-slug",
+        shipping: 1,
+      },
+      { new: true },
+    );
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.send).toHaveBeenCalledWith({
+      success: true,
+      message: "Product updated successfully",
+      products: {
+        _id: "1",
+        name: "Computer",
+        description: "A mock computer",
+        price: 10.99,
+        category: "1",
+        quantity: 10,
+        shipping: true,
+        photo: {
+          data: "mock-file",
+          contentType: "image/png",
+        },
+      },
+    });
+  });
+
+  it("should update product correctly if shipping is undefined", async () => {
+    const mockReq = {
+      params: { pid: "1" },
+      fields: {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+      },
+      files: {
+        photo: {
+          path: "/path/mock-file-1.png",
+          type: "image/png",
+          size: 100,
+        },
+      },
+    };
+    productModel.findByIdAndUpdate.mockResolvedValueOnce(
+      modelSaveWrapper({
+        _id: "1",
+        name: "Computer",
+        description: "A mock computer",
+        price: 10.99,
+        category: "1",
+        quantity: 10,
+        shipping: false,
+        photo: {
+          data: "mock-file-1",
+          contentType: "image/png",
+        },
+      }),
+    );
+
+    await updateProductController(mockReq, mockRes);
+
+    expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      "1",
+      {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+        slug: "computer-slug",
+        shipping: false,
+      },
+      { new: true },
+    );
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.send).toHaveBeenCalledWith({
+      success: true,
+      message: "Product updated successfully",
+      products: {
+        _id: "1",
+        name: "Computer",
+        description: "A mock computer",
+        price: 10.99,
+        category: "1",
+        quantity: 10,
+        shipping: false,
+        photo: {
+          data: "mock-file",
+          contentType: "image/png",
+        },
+      },
+    });
+  });
+
+  it("should update product correctly if photo is undefined", async () => {
+    const mockReq = {
+      params: { pid: "1" },
+      fields: {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+      },
+      files: {},
+    };
+    productModel.findByIdAndUpdate.mockResolvedValueOnce(
+      modelSaveWrapper({
+        _id: "1",
+        name: "Computer",
+        description: "A mock computer",
+        price: 10.99,
+        category: "1",
+        quantity: 10,
+        shipping: true,
+        photo: {
+          data: "mock-file",
+          contentType: "image/png",
+        },
+      }),
+    );
+
+    await updateProductController(mockReq, mockRes);
+
+    expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      "1",
+      {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+        slug: "computer-slug",
+        shipping: 1,
+      },
+      { new: true },
+    );
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.send).toHaveBeenCalledWith({
+      success: true,
+      message: "Product updated successfully",
+      products: {
+        _id: "1",
+        name: "Computer",
+        description: "A mock computer",
+        price: 10.99,
+        category: "1",
+        quantity: 10,
+        shipping: true,
+        photo: {
+          data: "mock-file",
+          contentType: "image/png",
+        },
+      },
+    });
+  });
+
+  it("should return error if product does not exist", async () => {
+    const mockReq = {
+      params: { pid: "1" },
+      fields: {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+      },
+      files: {},
+    };
+    productModel.findByIdAndUpdate.mockResolvedValueOnce(null);
+
+    await updateProductController(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(400);
+    expect(mockRes.send).toHaveBeenCalledWith({
+      success: false,
+      message: "Product does not exist",
+    });
+  });
+
+  it("should return error if server issues", async () => {
+    const mockReq = {
+      params: { pid: "1" },
+      fields: {
+        name: "Computer",
+        description: "A mock computer",
+        price: "10.99",
+        category: "1",
+        quantity: "10",
+        shipping: "1",
+      },
+      files: {},
+    };
+    productModel.findByIdAndUpdate.mockRejectedValue(
+      new Error("Update product error"),
+    );
+
+    await updateProductController(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.send).toHaveBeenCalledWith({
+      success: false,
+      error: new Error("Update product error"),
+      message: "Error in updating product",
+    });
+  });
+
+  describe("Field validation", () => {
+    it("should return error if name is missing", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          description: "A mock computer",
+          price: "10.99",
+          category: "1",
+          quantity: "10",
+          shipping: "1",
+        },
+        files: {},
+      };
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: false,
+        message: "Name is required",
+      });
+    });
+
+    it("should return error if description is missing", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          name: "Computer",
+          price: "10.99",
+          category: "1",
+          quantity: "10",
+          shipping: "1",
+        },
+        files: {},
+      };
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: false,
+        message: "Description is required",
+      });
+    });
+
+    it("should return error if price is missing", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          name: "Computer",
+          description: "A mock computer",
+          category: "1",
+          quantity: "10",
+          shipping: "1",
+        },
+        files: {},
+      };
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: false,
+        message: "Price is required",
+      });
+    });
+
+    it("should return error if category is missing", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          name: "Computer",
+          description: "A mock computer",
+          price: "10.99",
+          quantity: "10",
+          shipping: "1",
+        },
+        files: {},
+      };
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: false,
+        message: "Category is required",
+      });
+    });
+
+    it("should return error if quantity is missing", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          name: "Computer",
+          description: "A mock computer",
+          price: "10.99",
+          category: "1",
+          shipping: "1",
+        },
+        files: {},
+      };
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: false,
+        message: "Quantity is required",
+      });
+    });
+
+    it("should return error if photo size exceed limit", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          name: "Computer",
+          description: "A mock computer",
+          price: "10.99",
+          category: "1",
+          quantity: "10",
+          shipping: "1",
+        },
+        files: {
+          photo: {
+            path: "/path/mock-file.png",
+            type: "image/png",
+            size: 1000001,
+          },
+        },
+      };
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: false,
+        message: "Photo should be less then 1MB",
+      });
+    });
+
+    it("should do nothing if photo size equals to limit", async () => {
+      const mockReq = {
+        params: { pid: "1" },
+        fields: {
+          name: "Computer",
+          description: "A mock computer",
+          price: "10.99",
+          category: "1",
+          quantity: "10",
+          shipping: "1",
+        },
+        files: {
+          photo: {
+            path: "/path/mock-file.png",
+            type: "image/png",
+            size: 1000000,
+          },
+        },
+      };
+      productModel.findByIdAndUpdate.mockResolvedValueOnce(
+        modelSaveWrapper({
+          _id: "1",
+          name: "Computer",
+          description: "A mock computer",
+          price: 10.99,
+          category: "1",
+          quantity: 10,
+          shipping: true,
+          photo: {
+            data: "mock-file-1",
+            contentType: "image/png",
+          },
+        }),
+      );
+
+      await updateProductController(mockReq, mockRes);
+
+      expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+        "1",
+        {
+          name: "Computer",
+          description: "A mock computer",
+          price: "10.99",
+          category: "1",
+          quantity: "10",
+          shipping: "1",
+          slug: "computer-slug",
+          shipping: 1,
+        },
+        { new: true },
+      );
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        success: true,
+        message: "Product updated successfully",
+        products: {
+          _id: "1",
+          name: "Computer",
+          description: "A mock computer",
+          price: 10.99,
+          category: "1",
+          quantity: 10,
+          shipping: true,
           photo: {
             data: "mock-file",
             contentType: "image/png",
